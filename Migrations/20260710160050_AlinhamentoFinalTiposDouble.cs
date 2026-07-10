b60c54788cf0bb3d09f396b8ee174737b121d842
Pedro-Vitor7380 <pedrovtosatti@gmail.com>
2026-07-10 17:53:36 -0300

    melhoria de codigo

---
+++ Migrations/20260710160050_AlinhamentoFinalTiposDouble.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api_Produtos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlinhamentoFinalTiposDouble : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Pedidos");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ValorTotal",
+                table: "Pedidos",
+                type: "decimal(38,17)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,0)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataPedido",
+                table: "Pedidos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "valorTotal",
+                table: "Items",
+                type: "decimal(38,17)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,0)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PrecoUnitario",
+                table: "Items",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataPedido",
+                table: "Pedidos");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ValorTotal",
+                table: "Pedidos",
+                type: "decimal(18,0)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,17)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Pedidos",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "valorTotal",
+                table: "Items",
+                type: "decimal(18,0)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,17)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PrecoUnitario",
+                table: "Items",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
